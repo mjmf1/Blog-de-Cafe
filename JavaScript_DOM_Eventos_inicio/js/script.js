e97102dfddcbc149b2cd3ef8806365f663781388
mjmf1 <marcomora61@gmail.com>
2022-08-29 10:57:23 -0500

    Seleccionar Elementos con querySelectorAll

--- JavaScript_DOM_Eventos_inicio/js/script.js
+++ JavaScript_DOM_Eventos_inicio/js/script.js
@@ -1,6 +1,23 @@
-
+// querySelector
 const heading = document.querySelector('.header__texto h2') 
 
 heading.textContent = 'Blog de café con consejos y cursos 2'
 
-console.log(heading);
+//console.log(heading);
+
+//querySelectorAll
+
+const enlaces = document.querySelectorAll('.navegacion a');
+
+enlaces[0].textContent = 'nosotros2';
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion__enlace');
+
+//console.log(enlaces[0]);
+
+
+//getElementById  'no se usa tanto en las nuevas versiones'
+
+const heading2 = document.getElementById('heading');
+
+console.log(heading2);
